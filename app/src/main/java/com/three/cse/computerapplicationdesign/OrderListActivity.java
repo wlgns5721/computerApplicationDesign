7c9319ce62da338ff90a6507160684c1853fddef
jiyeon2659 <ddovobb69@gmail.com>
2018-06-23 17:16:01 +0900

    second

---
+++ app/src/main/java/com/three/cse/computerapplicationdesign/OrderListActivity.java
@@ -0,0 +1,35 @@
+package com.three.cse.computerapplicationdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class OrderListActivity extends AppCompatActivity {
+
+    TextView productID;
+    TextView productName;
+    TextView productPrice;
+    TextView productQuantity;
+
+    Button orderListResultButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_list);
+
+        productID = (TextView)findViewById(R.id.order_list_product_id);
+        productName = (TextView)findViewById(R.id.order_list_product_name);
+        productPrice = (TextView)findViewById(R.id.order_list_product_price);
+        productQuantity = (TextView)findViewById(R.id.order_list_product_quantity);
+
+        orderListResultButton = (Button)findViewById(R.id.order_list_result);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/three/cse/computerapplicationdesign/ProductQnaActivity.java
@@ -0,0 +1,46 @@
+package com.three.cse.computerapplicationdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class ProductQnaActivity extends AppCompatActivity {
+
+    WebView productQnaWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_qna);
+
+        productQnaWebView = (WebView)findViewById(R.id.product_qna_web_view);
+
+        WebSettings productQnaWebSettings = productQnaWebView.getSettings();
+        productQnaWebSettings.setAppCacheEnabled(false);
+
+        productQnaWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        productQnaWebView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+
+        productQnaWebView.loadUrl("https://www.naver.com/");
+
+    }
+
+
+
+}
