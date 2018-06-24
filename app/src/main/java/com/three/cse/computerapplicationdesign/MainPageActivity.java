6a1cd9afb2197522411f35f984cc87e54df1e9fc
sunghoon <sunghoon2454@gmail.com>
2018-06-24 16:24:24 +0900

    commit1

--- app/src/main/java/com/three/cse/computerapplicationdesign/MainPageActivity.java
+++ app/src/main/java/com/three/cse/computerapplicationdesign/MainPageActivity.java
@@ -2,8 +2,6 @@ package com.three.cse.computerapplicationdesign;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+
+import com.three.cse.computerapplicationdesign.activities.SearchResultActivity;
 
 public class MainPageActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
