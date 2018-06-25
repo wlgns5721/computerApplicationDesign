21a794f6955c6364e3c052f52a5c3b9014675925
sunghoon <sunghoon2454@gmail.com>
2018-06-25 21:33:56 +0900

    mainpage

--- app/src/main/java/com/three/cse/computerapplicationdesign/requests/MainPageItemRequest.java
+++ app/src/main/java/com/three/cse/computerapplicationdesign/requests/MainPageItemRequest.java
@@ -8,7 +8,6 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 
 public interface MainPageItemRequest {
-    @FormUrlEncoded
     @GET("/searchproductallbycount")
     Call<MainPageItemResponse> searchProduct();
 }
